446871456d7fcaf5f3f2c06c5f87e3617ec9d180
Airton <airton@a2comunicacao.com.br>
2010-04-27 12:09:18 -0300

    Inclusão da demonstração dos "botoes a la Google" ao A2FF

--- snippets/abas.js
+++
@@ -1,9 +0,0 @@
-/*
-	category: A2 Multimidia
-	name: Abas [JS]
-	toolTip: Abas [JS]
-*/
-	//Chamadas para os scripts de Abas com jQuery
-	//<script type="text/javascript" src="../js/jquery-1.3.2.js" ></script>
-	//<script type="text/javascript" src="../js/ui-abas.js" ></script>
-	//<script type="text/javascript" src="../js/abas.js" ></script>
