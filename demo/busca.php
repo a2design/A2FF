d0f0827dc99408b77015c8919c41badd99c227d1
airton <airton@a2comunicacao.com.br>
2010-09-21 16:16:47 -0300

    Alteracao nos arquivos busca.php e busca_head.html, e ahabilitato demonstracao

--- demo/busca.php
+++ demo/busca.php
@@ -4,18 +4,11 @@
 	<title>Busca</title>	
 	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
 	<meta name="author" content="A2 Comunicação" />	
-	<script type="text/javascript" charset="utf-8" src="../js/jquery.js"></script>
-	<script type="text/javascript" charset="utf-8" src="../snippets/busca.js"></script>
+	
+	<?php include('../snippets/busca_head.html');?>
 </head>
 <body id="teste">
-<div id="search">
-	<form action="javascript:void(0);">
-	  <label for="search">
-			<input type="text" class="input_text" name="search" alt="Digite sua busca" value="Digite sua busca"/>
-			<input type="submit" class="input_submit" value="Buscar" />
-	  </label>
-	</form>
-</div>
+	<?php include('../snippets/busca.html');?>
 </body>
 </html>
 
