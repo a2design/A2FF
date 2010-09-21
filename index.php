a490dc66d16f5eec9ac80b26e5351bc70f758c68
airton <airton@a2comunicacao.com.br>
2010-09-21 12:19:17 -0300

    Habilitado demonstracao do CSS3 Gradient Buttons no index

--- index.php
+++ index.php
@@ -725,7 +725,7 @@
 									<a href="#">CSS</a></td>
 								<td class="multimidia">Multimídia</td>
 								<td class="status"><span class="complete" title="Funcionamento completo">Completo</span></td>
-								<td class="demo disable"><a href="#">Demonstração</a></td>
+								<td class="demo"><a href="demo/google-maps-icons.php">Demonstração</a></td>
 								<td class="download"><a href="/prod/sitemaps/descritivo.xls">Download</a></td>
 							</tr>
 							<tr class="odd gradeA">
@@ -737,7 +737,7 @@
 									<a href="#">CSS</a></td>
 								<td class="multimidia">Multimídia</td>
 								<td class="status"><span class="complete" title="Funcionamento completo">Completo</span></td>
-								<td class="demo disable"><a href="#">Demonstração</a></td>
+								<td class="demo"><a href="demo/css3-gradient-buttons.php">Demonstração</a></td>
 								<td class="download"><a href="/prod/sitemaps/descritivo.xls">Download</a></td>
 							</tr>
 							<tr class="odd gradeA">
