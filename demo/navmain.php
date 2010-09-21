7dc0b52bf27f42d7814ab4d0093944cea44e9e5a
airton <airton@a2comunicacao.com.br>
2010-09-21 10:11:35 -0300

    Correcao no modulo de slideshow, adicionado jquery.cycle.js na pasta que estava falntando

--- demo/navmain.php
+++ demo/navmain.php
@@ -3,53 +3,14 @@
 <head>
 	<title>Navmain</title>	
 	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-	<meta name="author" content="A2 Comunicação" />	
-	<script type="text/javascript" src="../js/jquery-1.4.2.min.js" ></script>
-	<script type="text/javascript" src="../js/superfish.js" ></script>
+	<meta name="author" content="A2 Comunicaï¿½ï¿½o" />	
 	<link rel="stylesheet" type="text/css" href="../snippets/navmain.css" media="all" />	
-	<script type="text/javascript">
-		$(function(){
-			$("#nav_main ul").superfish();
-			$("#nav_main ul .item01 .submenu li:last, #nav_main ul .item03 .submenu li:last, #nav_main ul .item06 .submenu li:last, #nav_main ul .item08 .submenu li:last").css('border-bottom', 'none');
-		});
-	</script>
-
+  
+  <?php include('../snippets/navmain_head.html') ?>
+  
 </head>
 <body id="teste">
-<div id="nav_main">
-	<ul>
-		<li class="item01"><a href="javascript:void(0);">item01</a>
-			<ul class="submenu">
-					<li class="item01"><a href="javascript:void(0);">sub01</a></li>
-					<li class="item02"><a href="javascript:void(0);">sub02</a></li>
-			</ul>
-		</li>
-		<li class="item02"><a href="javascript:void(0);">item02</a></li>
-		<li class="item03"><a href="javascript:void(0);">item03</a>
-			<ul class="submenu">
-					<li class="item01"><a href="javascript:void(0);">sub01</a></li>
-					<li class="item02"><a href="javascript:void(0);">sub02</a></li>
-			</ul>		
-		</li>
-		<li class="item04"><a href="javascript:void(0);">item04</a></li>
-		<li class="item05"><a href="javascript:void(0);">item05</a></li>
-		<li class="item06"><a href="javascript:void(0);">item06</a>
-			<ul class="submenu">
-					<li class="item01"><a href="javascript:void(0);">sub01</a></li>
-					<li class="item02"><a href="javascript:void(0);">sub02</a></li>
-			</ul>		
-		</li>
-		<li class="item07"><a href="javascript:void(0);">item07</a></li>
-		<li class="item08"><a href="javascript:void(0);">item08</a>
-			<ul class="submenu">
-					<li class="item01"><a href="javascript:void(0);">sub01</a></li>
-					<li class="item02"><a href="javascript:void(0);">sub02</a></li>
-			</ul>		
-		</li>
-		<li class="item09"><a href="javascript:void(0);">item09</a></li>
-		<li class="item10"><a href="javascript:void(0);">item10</a></li>
-	</ul>
-</div>
+  <?php include('../snippets/navmain.html') ?>
 </body>
 </html>
 
