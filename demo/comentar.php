539697b0efb12a4b4041839d4326cd77128665c5
airton <airton@a2comunicacao.com.br>
2010-09-23 14:46:22 -0300

    Alteracao no arquivo comentar.php

--- demo/comentar.php
+++ demo/comentar.php
@@ -8,25 +8,8 @@
 	<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />	
 	<link rel="alternate" type="application/rss+xml" title="Comentar RSS" href="#" />	
 </head>
-<body id="">
-		<div id="comentarios">
-			<div class="col1">
-				<h3>Comente</h3>
-				<p>Para comentar esta notícia você precisa estar cadastrado, faça o login e deixe seu comentário.</p>
-				<p class="action"><a href="#item01" class="login">Login</a></p>			
-			</div>
-			<div class="col2">
-				<h3>Faça seu Cadastro</h3>
-				<p>Cadastre-se para poder comentar as notícias além de receber conteúdos exclusivos.</p>
-				<p class="action"><a href="/sis/cadastrosp.php">Cadastrar</a></p>
-			</div>			
-			<div class="col3">
-				<h3>Atualize seu dados</h3>
-				<p>Mantenha seus dados cadastrais atualizados para ampliar sua participação junto ao portal. </p>
-				<p class="action"><a href="#item03" class="atualizar">Atualizar</a></p>
-			</div>
-            
-		</div>
+<body id="teste">
+	<?php include('../snippets/comentar.html');?>
 </body>
 </html>
 
